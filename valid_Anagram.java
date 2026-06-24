fb68ee33fa8dded84f5edcd889a26a073874b65c
Akshay Kumar Meena <akshay.kumarmeena@handyhome.in>
2026-06-24 10:57:17 +0530

    Create valid_Anagram.java

---
+++ valid_Anagram.java
@@ -0,0 +1,33 @@
+Given two strings s and t, return true if t is an anagram of s (uses exactly the same characters, same frequency, just rearranged), and false otherwise.
+    
+  Example:
+          Input: s = "anagram", t = "nagaram"
+          Output: true
+          
+          Input: s = "rat", t = "car"
+          Output: false
+
+
+  // Method 1
+  
+  public boolean isAnagram(String s, String t){
+    if(s.length() != t.length()) return false;
+
+    char[] s1 = s.toCharArray();
+    char[] t1 = t.toCharArray();
+
+    Arrays.sort(s1);
+    Arrays.sort(t1);
+
+    for(int i=0; i<s1.length; i++){
+        if(s1[i] != t1[i]) return false;
+    }
+
+    return true;
+}
+
+
+
+
+
+  
