9493e3eaf5c0add008b1d23e6432a0ab181286b9
Akshay Kumar Meena <akshay.kumarmeena@handyhome.in>
2026-06-24 11:29:29 +0530

    Create Minimum Size Subarray Sum.java

---
+++ Minimum Size Subarray Sum.java
@@ -0,0 +1,16 @@
+Given an array of positive integers nums and a positive integer target, return the length of the smallest contiguous subarray whose sum is greater than or equal to target. If no such subarray exists, return 0.
+          
+  
+  Example:
+          Input: target = 7, nums = [2, 3, 1, 2, 4, 3]
+          Output: 2
+          Explanation: [4, 3] has the smallest length with sum >= 7
+          
+          Input: target = 4, nums = [1, 4, 4]
+          Output: 1
+          
+          Input: target = 11, nums = [1, 1, 1, 1, 1, 1, 1, 1]
+          Output: 0
+
+
+  
