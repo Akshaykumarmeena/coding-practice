893ceaa1ad3c91bcb7ea4664e40739496d908064
Akshay Kumar Meena <akshay.kumarmeena@handyhome.in>
2026-06-24 10:59:21 +0530

    Create group_Anagrams.java

---
+++ group_Anagrams.java
@@ -0,0 +1,33 @@
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    
+  Example:
+    Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
+
+
+
+static List<List<String>> groupAnagram(List<String> input){
+        
+         HashMap<String,List<String>> map = new HashMap<>();
+         
+         for(int i=0;i<input.size();i++){
+             char[] chars = input.get(i).toCharArray();
+             Arrays.sort(chars);
+             
+             String sorted = new String(chars);
+             
+             if(!map.containsKey(sorted)){
+                 map.put(sorted, new ArrayList<String>());
+             }
+                 map.get(sorted).add(input.get(i));
+         }
+         
+         List<List<String>> output = new ArrayList<>(map.values());
+         
+         return output;
+        
+        
+    }
+
+
+i took a lot of hints of functions of hashmap and lists but not for the logics and flow
