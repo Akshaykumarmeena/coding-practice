1fccc2186b16236938a929a5d9efd77eef7b9879
Akshay Kumar Meena <akshaykumarmeena16@example.com>
2026-07-22 22:02:35 +0530

    Added Snakes and Ladders

---
+++ Machine Coding/snakesAndLadders/entities/BoardEntity.java
@@ -0,0 +1,18 @@
+abstract class BoardEntity {
+    private final int start;
+    private final int end;
+
+    public BoardEntity(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart(){
+        return this.start;
+    }
+
+    public int getEnd(){
+        return this.end;
+    }
+
+}
---
+++ Machine Coding/snakesAndLadders/entities/Ladder.java
@@ -0,0 +1,10 @@
+class Ladder extends BoardEntity{
+    public Ladder(int start, int end){
+        super(start, end);
+        if(start >= end){
+            throw new IllegalArgumentException(
+                "Ladder position must be at a lower position than its top"
+            );
+        }
+    }
+}
